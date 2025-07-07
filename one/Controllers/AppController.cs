8541613ba3f4ca4b43f61db1ec16522cc90b5fe3
tim <timself@qq.com>
2025-07-07 08:37:14 +0800

    init

---
+++ one/Controllers/AppController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace one.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AppController(ILogger<AppController> logger) : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            if (logger.IsEnabled(LogLevel.Information)) { logger.LogInformation("ping"); }
+            return Ok("TONG");
+        }
+    }
+}
